e1c323b6081370abfe65be0bab24da3faceb5251
raquelpz <rpz.ra.chel@gmail.com>
2025-05-01 15:56:25 +0200

    Condicionales

--- ejerciciorafa/script.js
+++ ejerciciorafa/script.js
@@ -1,34 +1,80 @@
-// 1. coger el valor de la cajita 
+// Escribir a la par
 
-// 2. Coger el div de violeta
-
-// 3. poner en el div de violeta el valor que hemos cogido de la cajita
-
-
-const elNombre = document.querySelector("input");
-let elValor;
-elNombre.addEventListener("keyup", (event) =>{
-    elValor = elNombre.value;
-    const laVio = document.querySelector(".violeta");
-    laVio.innerHTML = elValor;
-    comprobarValor();
+const guion = " · ";
+const colorI = document.querySelector("#color_i");
+let valorColor;
+colorI.addEventListener("keyup", (event) =>{
+    valorColor = colorI.value;
+    const laMuestra = document.querySelector("#color_m");
+    laMuestra.innerHTML = guion + valorColor + guion;
+    comprobarValorColor();
 });
 
-// 1. Guardar el nombre de Fermin
+const signoI = document.querySelector("#signo_i");
+let valorSigno;
+signoI.addEventListener("keyup", (event) =>{
+    valorSigno = signoI.value;
+    const laMuestra = document.querySelector("#signo_m");
+    laMuestra.innerHTML = guion + valorSigno + guion;
+    comprobarValorSigno();
+});
 
-// 2. Comparar fermin con elValor
+const estacionI = document.querySelector("#estacion_i");
+let valorEstacion;
+estacionI.addEventListener("keyup", (event) =>{
+    valorEstacion = estacionI.value;
+    const laMuestra = document.querySelector("#estacion_m");
+    laMuestra.innerHTML = guion + valorEstacion + guion;
+    comprobarValorEstacion();
+});
 
-// 3. si es igual: subtitulo se vulve ver (h1)
+//Cabio de colores
 
-const elGato = "Fermín";
-const parrafo =document.querySelector("p");
-function comprobarValor() {
-    if ( elValor === elGato ) {
-        parrafo.classList.remove("incorrecto");
-        parrafo.classList.add("correcto");
+const elColor = "Turquesa";
+const colorP = document.querySelector("#color_p");
+
+
+function comprobarValorColor() {
+    if ( valorColor === elColor ) {
+        colorP.classList.remove("incorrecto");
+        colorP.classList.add("correcto");
+        caraColor.innerHTML = cara;
     } else {
-        parrafo.classList.remove("correcto");
-        parrafo.classList.add("incorrecto");
+        colorP.classList.remove("correcto");
+        colorP.classList.add("incorrecto");
     }
 };
+
+const elSigno = "Cáncer";
+const signoP = document.querySelector("#signo_p");
+
+function comprobarValorSigno(){
+    if ( valorSigno === elSigno ) {
+        signoP.classList.remove("incorrecto");
+        signoP.classList.add("correcto");
+        caraSigno.innerHTML = cara;
+    } else {
+        signoP.classList.remove("correcto");
+        signoP.classList.add("incorrecto");
+    }
+};
+
+const laEstacion = "Verano";
+const estacionP = document.querySelector("#estacion_p");
+
+function comprobarValorEstacion(){
+    if ( valorEstacion === laEstacion ) {
+        estacionP.classList.remove("incorrecto");
+        estacionP.classList.add("correcto");
+        caraEstacion.innerHTML = cara;
+    } else {
+        estacionP.classList.remove("correcto");
+        estacionP.classList.add("incorrecto");
+    }
+};
+
+const cara = "☻";
+const caraColor = document.querySelector ("#color_d");
+const caraSigno = document.querySelector ("#signo_d");
+const caraEstacion = document.querySelector ("#estacion_d");
 
