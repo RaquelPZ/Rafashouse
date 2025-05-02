c5f1fc486f152c2c86f40951f602309974dcb324
raquelpz <rpz.ra.chel@gmail.com>
2025-05-02 20:25:55 +0200

    probando lvls

--- ejerciciorafa/script.js
+++ ejerciciorafa/script.js
@@ -1,3 +1,7 @@
+let elContador = 0;
+const contadorC = document.querySelector("#puntos");
+const elPremio = document.querySelector("#premio");
+
 // Escribir a la par
 
 const guion = " · ";
@@ -28,10 +32,35 @@ estacionI.addEventListener("keyup", (event) =>{
     comprobarValorEstacion();
 });
 
+const persoI = document.querySelector("#perso_i");
+let valorPerso;
+persoI.addEventListener("keyup", (event) =>{
+    valorPerso = persoI.value;
+    const laMuestra = document.querySelector("#perso_m");
+    laMuestra.innerHTML = guion + valorPerso + guion;
+    comprobarValorPerso();
+});
+
+const luzI = document.querySelector("#luz_i");
+let valorLuz;
+luzI.addEventListener("keyup", (event) =>{
+    valorLuz = luzI.value;
+    const laMuestra = document.querySelector("#luz_m");
+    laMuestra.innerHTML = guion + valorLuz + guion;
+    comprobarValorLuz();
+});
+
 //Cabio de colores
 
 const elColor = "Turquesa";
 const colorP = document.querySelector("#color_p");
+
+const cara = "☻";
+const caraColor = document.querySelector ("#color_d");
+const caraSigno = document.querySelector ("#signo_d");
+const caraEstacion = document.querySelector ("#estacion_d");
+const caraPerso = document.querySelector ("#perso_d");
+const caraLuz = document.querySelector ("#luz_d");
 
 
 function comprobarValorColor() {
@@ -39,6 +68,8 @@ function comprobarValorColor() {
         colorP.classList.remove("incorrecto");
         colorP.classList.add("correcto");
         caraColor.innerHTML = cara;
+        elContador = elContador + 1;
+        colorI.disabled = true;
     } else {
         colorP.classList.remove("correcto");
         colorP.classList.add("incorrecto");
@@ -53,6 +84,8 @@ function comprobarValorSigno(){
         signoP.classList.remove("incorrecto");
         signoP.classList.add("correcto");
         caraSigno.innerHTML = cara;
+        elContador = elContador + 1;
+        signoI.disabled = true;
     } else {
         signoP.classList.remove("correcto");
         signoP.classList.add("incorrecto");
@@ -67,14 +100,72 @@ function comprobarValorEstacion(){
         estacionP.classList.remove("incorrecto");
         estacionP.classList.add("correcto");
         caraEstacion.innerHTML = cara;
+        elContador = elContador + 1;
+        estacionI.disabled = true;
     } else {
         estacionP.classList.remove("correcto");
         estacionP.classList.add("incorrecto");
     }
 };
 
-const cara = "☻";
-const caraColor = document.querySelector ("#color_d");
-const caraSigno = document.querySelector ("#signo_d");
-const caraEstacion = document.querySelector ("#estacion_d");
+const laPerso = "Siete";
+const persoP = document.querySelector("#perso_p");
 
+function comprobarValorPerso(){
+    if ( valorPerso === laPerso ) {
+        persoP.classList.remove("incorrecto");
+        persoP.classList.add("correcto");
+        caraPerso.innerHTML = cara;
+        elContador = elContador + 1;
+        persoI.disabled = true;
+    } else {
+        persoP.classList.remove("correcto");
+        persoP.classList.add("incorrecto");
+    }
+};
+
+const laLuz = "Rodilla";
+const luzP = document.querySelector("#luz_p");
+
+function comprobarValorLuz(){
+    if ( valorLuz === laLuz ) {
+        luzP.classList.remove("incorrecto");
+        luzP.classList.add("correcto");
+        caraLuz.innerHTML = cara;
+        elContador = elContador + 1;
+        luzI.disabled = true;
+    } else {
+        luzP.classList.remove("correcto");
+        luzP.classList.add("incorrecto");
+    }
+};
+
+const elFin = document.querySelector ("#fin");
+const elTiempo = setTimeout (() => {
+    elFin.classList.remove ('off');
+    elFin.classList.add ('on');
+}, 1*60*1000);
+
+// const laIgualdad = valorColor === elColor && valorSigno === elSigno &&  valorEstacion === laEstacion && valorLuz === laLuz && valorPerso === laPerso;
+
+document.addEventListener("keyup", (event) =>{
+    contadorC.innerHTML = elContador;
+    if ( elContador === 5 ) {
+        clearTimeout (elTiempo);
+        console.log ('Todo correcto');
+        elPremio.classList.remove ('off');
+        elPremio.classList.add ('on2');
+    }
+});
+const colorBueno = "#0AE6CC";
+const colorElegido = document.getElementById('colorFavorito').value; // Esto devolverá un valor hexadecimal, como "#ff0000"
+const btnAceptar = document.querySelector ("#btn_22");
+
+btnAceptar.addEventListener("click", (event) =>{
+    if ( colorBueno === colorElegido ) {
+        // clearTimeout (elTiempo);
+        btnAceptar.classList.add("correcto");
+    } else {
+        btnAceptar.classList.add("incorrecto");
+    }
+});
